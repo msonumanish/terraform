60bad82bb376a6298f8af8a1ae0c913d5d6e0023
msonumanish <32475118+msonumanish@users.noreply.github.com>
2023-09-10 05:39:29 +0530

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -1,3 +1,14 @@
 locals {
     arr = ["host1", "host2"]
 }
+
+provider "aws" {
+
+region = "ap-south-1"
+}
+
+resource "aws_instance" "app-dev" {
+  ami = ami-06f621d90fa29f6d0
+  instance_type = "t2.micro"
+
+}
